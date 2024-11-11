d7563fd9c5918b6d5e2a73b4fd22b52a9e34e196
多多 <130138203+alvaroduo@users.noreply.github.com>
2024-11-11 22:43:14 +0800

    fix multiple apis and other problems

---
+++ Assets/Script/audioPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioClip plantSound; // Public field for the jump sound effect
+    private AudioSource plantAudioSource; // For the plant sound
+
+    void Start()
+    {
+        plantAudioSource = gameObject.AddComponent<AudioSource>(); // Create a new AudioSource 
+
+        // Assign the jump sound effect if it was set in the Inspector
+        if (plantSound != null)
+        {
+            plantAudioSource.clip = plantSound;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Alien")) // Check if the plant collided with the alien
+        {
+            plantAudioSource.Play();
+        }
+    }
+}
