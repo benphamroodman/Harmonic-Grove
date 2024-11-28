f9853137d900bdefe5772a377fa738b1c06362b5
shareneelie <66663392+shareneelie@users.noreply.github.com>
2024-11-28 22:12:20 +0800

    Add function to generate plants on collision

---
+++ Assets/Script/PlantGeneratingContoller.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGeneratingContoller : MonoBehaviour
+{
+    public GameObject flowerPrefab;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("pencil"))
+        {
+            Debug.Log("Collide with pencil!");
+            Vector3 spawnPosition = new Vector3(-4.85f, 0.8f, 3f);
+            Instantiate(flowerPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
