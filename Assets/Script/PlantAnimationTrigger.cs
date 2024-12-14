6f26338a732bd7335fe94406b790eb7d6870a1c2
Lynn-Chuang-01 <m11310305@gapps.ntust.edu.tw>
2024-12-15 00:16:15 +0800

    fix music

--- Assets/Script/PlantAnimationTrigger.cs
+++ Assets/Script/PlantAnimationTrigger.cs
@@ -1,24 +1,25 @@
-//using UnityEngine;
-
-//public class PlantAnimationTrigger : MonoBehaviour {
-//    private Animator animator;
-//    private bool isActive = false;  // 篈ち传北ガ狶
-
-//    void Start() {
-//        animator = GetComponent<Animator>();  // 莉Animator舱ン
-//    }
-
-//    private void OnTriggerEnter(Collider other) {
-//        if (other.CompareTag("wand"))  // 浪琩窱疾琌wand
-//        {
-//            isActive = !isActive;  // ち传篈
-//            animator.SetBool("isActive", isActive);  // 穝笆礶把计
-
-//            Debug.Log(isActive ? "play animation" : "back to Idle state");
-//        }
-//    }
-//}
 using UnityEngine;
+
+public class PlantAnimationTrigger : MonoBehaviour {
+    private Animator animator;
+    private bool isActive = false;  // 篈ち传北ガ狶
+
+    void Start() {
+        animator = GetComponent<Animator>();  // 莉Animator舱ン
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("wand"))  // 浪琩窱疾琌wand
+        {
+            isActive = !isActive;  // ち传篈
+            animator.SetBool("isActive", isActive);  // 穝笆礶把计
+
+            Debug.Log(isActive ? "play animation" : "back to Idle state");
+        }
+    }
+}
+
+/*using UnityEngine;
 
 public class PlantAnimationTrigger : MonoBehaviour
 {
@@ -61,3 +62,4 @@ public class PlantAnimationTrigger : MonoBehaviour
     }
 }
 
+*/
